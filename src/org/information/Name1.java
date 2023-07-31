dc078754f6f4f7bff02a1756fcf33a6c36e958ae
GladysGabriel <lgssujithra@gmail.com>
2023-07-31 13:15:48 -0700

    first commit

---
+++ src/org/information/Name1.java
@@ -0,0 +1,127 @@
+package org.information;
+
+public class Name1 {
+	public static void main(String[] args) {
+		String name1 = "Anitha";
+		int age1 = 28;
+		String school1 = "ABC School";
+		String location1 = "Chennai";
+		
+		String name2 = "Alice";
+		int age2 = 26;
+		String school2 = "ABC School";
+		String location2 = "LosAngeles";
+		
+		String name3 = "Joy";
+		int age3 = 24;
+		String school3 = "XYZ School";
+		String location3 = "NewYork";
+		
+		String name4 = "Blessy";
+		int age4 = 23;
+		String school4 = "def School";
+		String location4 = "Trichy";
+		
+		String name5 = "Clara";
+		int age5 = 22;
+		String school5 = "GHI School";
+		String location5 = "Goa";
+		
+		String name6 = "Daisy";
+		int age6 = 21;
+		String school6 = "JKL School";
+		String location6 = "Pune";
+		
+		String name7 = "Eesha";
+		int age7 = 27;
+		String school7 = "LMN School";
+		String location7 = "Canada";
+		
+		String name8 = "Flora";
+		int age8 = 25;
+		String school8 = "OPEQ School";
+		String location8 = "Russia";
+		
+		String name9 = "Gosha";
+		int age9 = 22;
+		String school9 = "RST School";
+		String location9 = "London";
+		
+		String name10 = "Hyena";
+		int age10 = 23;
+		String school10 = "UV School";
+		String location10 = "seria";
+		
+		String name11 = "Iana";
+		int age11 = 24;
+		String school11 = "Walt School";
+		String location11 = "Ceylon";
+		
+		String name12 = "Jeny";
+		int age12 = 26;
+		String school12 = "Apec School";
+		String location12 = "Chennai";
+		
+		String name13 = "Krish";
+		int age13 = 23;
+		String school13 = "Quipe School";
+		String location13 = "Tirupur";
+		
+		String name14 = "Luna";
+		int age14 = 25;
+		String school14 = "Bora School";
+		String location14 = "Visag";
+		
+		String name15 = "Marok";
+		int age15 = 22;
+		String school15 = "Dilraba School";
+		String location15 = "Karur";
+		
+		String name16 = "Nischal";
+		int age16 = 28;
+		String school16 = "Meera School";
+		String location16 = "Delhi";
+		
+		String name17 = "Oberk";
+		int age17 = 24;
+		String school17 = "Bharath School";
+		String location17 = "Jaipur";
+		
+		String name18 = "Priyan";
+		int age18 = 23;
+		String school18 = "WXY School";
+		String location18 = "Shimla";
+		
+		String name19 = "Rishi";
+		int age19 = 25;
+		String school19 = "Selvan School";
+		String location19= "Noida";
+		
+		String name20 = "Siddhu";
+		int age20 = 21;
+		String school20 = "Port School";
+		String location20 = "Imphal";
+		
+		System.out.println("PersonalDetails of 20 People:");
+		System.out.println("\nPerson1:"+"\nName: "+ name1 + "\n" + "Age: " + age1 + "\n" + "School: " + school1 + "\n" + "Location: " + location1);
+		System.out.println("\nPerson2:"+"\nName: "+ name2 + "\n" + "Age: " + age2 + "\n" + "School: " + school2 + "\n" + "Location: " + location2);
+		System.out.println("\nPerson3:"+"\nName: "+ name3 + "\n" + "Age: " + age3 + "\n" + "School: " + school3 + "\n" + "Location: " + location3);
+		System.out.println("\nPerson4:"+"\nName: "+ name4 + "\n" + "Age: " + age4 + "\n" + "School: " + school4 + "\n" + "Location: " + location4);
+		System.out.println("\nPerson5:"+"\nName: "+ name5 + "\n" + "Age: " + age5 + "\n" + "School: " + school5 + "\n" + "Location: " + location5);
+		System.out.println("\nPerson6:"+"\nName: "+ name6 + "\n" + "Age: " + age6 + "\n" + "School: " + school6 + "\n" + "Location: " + location6);
+		System.out.println("\nPerson7:"+"\nName: "+ name7 + "\n" + "Age: " + age7 + "\n" + "School: " + school7 + "\n" + "Location: " + location7);
+		System.out.println("\nPerson8:"+"\nName: "+ name8 + "\n" + "Age: " + age8 + "\n" + "School: " + school8 + "\n" + "Location: " + location8);
+		System.out.println("\nPerson9:"+"\nName: "+ name9 + "\n" + "Age: " + age9 + "\n" + "School: " + school9 + "\n" + "Location: " + location9);
+		System.out.println("\nPerson10:"+"\nName: "+ name10 + "\n" + "Age: " + age10+ "\n" + "School: " + school10+ "\n" + "Location: " + location10);
+		System.out.println("\nPerson11:"+"\nName: "+ name11+ "\n" + "Age: " + age11 + "\n" + "School: " + school11+ "\n" + "Location: " + location11);
+		System.out.println("\nPerson12:"+"\nName: "+ name12+ "\n" + "Age: " + age12+ "\n" + "School: " + school12+ "\n" + "Location: " + location12);
+		System.out.println("\nPerson13:"+"\nName: "+ name13+ "\n" + "Age: " + age13+ "\n" + "School: " + school13+ "\n" + "Location: " + location13);
+		System.out.println("\nPerson14:"+"\nName: "+ name14+ "\n" + "Age: " + age14+ "\n" + "School: " + school14+ "\n" + "Location: " + location14);
+		System.out.println("\nPerson15:"+"\nName: "+ name15+ "\n" + "Age: " + age15+ "\n" + "School: " + school15+ "\n" + "Location: " + location15);
+		System.out.println("\nPerson16:"+"\nName: "+ name16+ "\n" + "Age: " + age16+ "\n" + "School: " + school16+ "\n" + "Location: " + location16);
+		System.out.println("\nPerson17:"+"\nName: "+ name17+ "\n" + "Age: " + age17+ "\n" + "School: " + school17+ "\n" + "Location: " + location17);
+		System.out.println("\nPerson18:"+"\nName: "+ name18+ "\n" + "Age: " + age18+ "\n" + "School: " + school18+ "\n" + "Location: " + location18);
+		System.out.println("\nPerson19:"+"\nName: "+ name19+ "\n" + "Age: " + age19+ "\n" + "School: " + school19+ "\n" + "Location: " + location19);
+		System.out.println("\nPerson20:"+"\nName: "+ name20+ "\n" + "Age: " + age20+ "\n" + "School: " + school20+ "\n" + "Location: " + location20);
+	}	
+}
